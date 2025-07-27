fc7dccca71babcf2674cf18e7bea71d76d8519d8
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 00:10:54 +0530

    Create exp3(1).cpp

---
+++ exp3(1).cpp
@@ -0,0 +1,30 @@
+//Shivangi Singh
+//prn 24070123148
+//B3
+#include <iostream>
+using namespace std ;
+int main(){
+    cout << "input the number " ; 
+    int num;
+    cin >> num ; 
+    if (num<0){  
+        cout << "number is negative" ; 
+    }
+    else if (num>0)
+    {
+        cout << "number is positive" ;
+    }
+    else{
+    cout << "number is zero" ;}
+    
+return 0;
+}
+
+/* Output
+input the number 21
+number is positive
+input the number 0
+number is zero
+input the number -18
+number is negative
+ */
